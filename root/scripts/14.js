d882ea8f638b830d656ebf578db75a774f90ae2e
Piotr <piotr_as@yahoo.com>
2018-08-23 13:08:38 +0200

    dodane wszystkie funkcjonalności z jQuery

--- root/scripts/14.js
+++ root/scripts/14.js
@@ -1,5 +1,11 @@
 $(function () {
     $('#larger').click(function () {
         $('h2').toggleClass('big');
-    })
+    });
+    $('#smaller').click(function () {
+        $('h2').toggleClass('small');
+    });
+    $('#border').click(function () {
+        $('h2').toggleClass('bdr');
+    });
 });
